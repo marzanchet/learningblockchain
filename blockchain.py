b2efd4f6fcdcae44a81d3df713edb0667288b4ce
Marcelo Zanchet <82295942+marzanchet@users.noreply.github.com>
2021-10-20 15:44:18 -0300

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+from block import Block 
+
+class Blockchain:
+    """
+    Blockchain: a public ledger of transactions
+    """
+    def __init__(self):
+        self.chain = []
+
+    def add_block(self, data):
+        self.chain.append(Block(data))
+
+    def __repr__(self):
+        return f'Blockchain {self.chain}'
+
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+
+    print(blockchain)
+    print(f'block.py __name__: {__name__}')
+
+if __name__ == '__main__':
+    main()
