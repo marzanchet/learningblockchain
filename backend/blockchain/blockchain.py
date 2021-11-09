7ce8967cbf5ff639c4cfd86de7956565b3888cc5
Marcelo Zanchet <82295942+marzanchet@users.noreply.github.com>
2021-11-09 00:38:50 -0300

    Update blockchain.py

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -2,7 +2,8 @@ from backend.blockchain.block import Block
 
 class Blockchain:
     """
-    Blockchain: a public ledger of transactions
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - data sets of transactions
     """
     def __init__(self):
         self.chain = [Block.genesis()]
@@ -11,13 +12,13 @@ class Blockchain:
         self.chain.append(Block.mine_block(self.chain[-1], data))
 
     def __repr__(self):
-        return f'Blockchain {self.chain}'
+        return f'Blockchain: {self.chain}'
 
     def replace_chain(self, chain):
         """
         Replace the local chain with the incoming one if the following applies:
-        - The incoming chain is longer than the local one
-        - The incoming chain is formatted properly.
+          - The incoming chain is longer than the local one.
+          - The incoming chain is formatted properly.
         """
         if len(chain) <= len(self.chain):
             raise Exception('Cannot replace. The incoming chain must be longer.')
@@ -29,14 +30,13 @@ class Blockchain:
 
         self.chain = chain
 
-
     @staticmethod
     def is_valid_chain(chain):
         """
         Validate the incoming chain.
-        Enforce the following rules of the blockchain
-            - the chain must start with the genesis block
-            - blocks must be formatted correctly
+        Enforce the following rules of the blockchain:
+          - the chain must start with the genesis block
+          - blocks must be formatted correctly
         """
         if chain[0] != Block.genesis():
             raise Exception('The genesis block must be valid')
@@ -47,15 +47,13 @@ class Blockchain:
             Block.is_valid_block(last_block, block)
 
 
-
 def main():
     blockchain = Blockchain()
     blockchain.add_block('one')
     blockchain.add_block('two')
 
     print(blockchain)
-    print(f'block.py __name__: {__name__}')
-    
+    print(f'blockchain.py ___name__: {__name__}')
 
 if __name__ == '__main__':
     main()
