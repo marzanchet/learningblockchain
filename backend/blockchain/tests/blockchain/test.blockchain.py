4556ffc8d82f46d03d8be0a9221035a7dab95acf
Marcelo Zanchet <82295942+marzanchet@users.noreply.github.com>
2021-11-09 00:25:18 -0300

    updates

--- backend/blockchain/tests/blockchain/test.blockchain.py
+++ backend/blockchain/tests/blockchain/test.blockchain.py
@@ -1,3 +1,5 @@
+import pytest
+
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA, Block
 
@@ -13,10 +15,15 @@ def test_add_block():
 
     assert blockchain.chain[-1].data == data
 
-def test_is_valid_chain():
+@pytest.fixture
+
+def blockchain_three_blocks():
     blockchain = Blockchain()
-    
     for i in range(3):
         blockchain.add_block(i)
+    return blockchain
 
-    Blockchain.is_valid_chain(blockchain.chain)
+def test_is_valid_chain(blockchain_three_blocks):
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
+
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
